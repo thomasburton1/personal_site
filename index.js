0280676136317952328f3677bd8de136fa5455d8
thomasburton1 <thomas.s.burton11@gmail.com>
2022-09-19 13:33:02 -0600

    converted to nodejs project

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express'),
+    http = require('http');
+
+var path = require('path');
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+var server = http.createServer(app);
+
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+    res.render('landing');
+});
+
+app.get('/projects', function (req, res) {
+    res.render('projects');
+});
+
+app.get('/resume', function (req, res) {
+    res.render('resume');
+});
+
+app.listen(4000);
+console.log("Listening at localhost:4000");
